75cbea8e1fbd20bb21c4b60628e0c5bfe0760404
Jyoti Gupta <jyotigup2025@gmail.com>
2026-07-12 12:22:20 +0530

    feat: add database configuration and environment variables to .env file

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,9 +1,21 @@
-const { Sequelize } = require("sequelize");
+const Sequelize = require("sequelize");
 
-const db = new Sequelize("mysql://root:DQjecVQflyAZBagrdqBUGTHpQuzwnBPJ@hayabusa.proxy.rlwy.net:36889/railway", {
-    dialect: "mysql",
-    logging: false,
-});
+let db;
+if (process.env.MYSQL_URL || process.env.DATABASE_URL) {
+    db = new Sequelize(process.env.MYSQL_URL || process.env.DATABASE_URL, {
+        dialect: 'mysql',
+    });
+} else {
+    db = new Sequelize(
+        process.env.DB_NAME || process.env.MYSQLDATABASE,
+        process.env.DB_USER || process.env.MYSQLUSER,
+        process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+        {
+            host: process.env.DB_HOST || process.env.MYSQLHOST,
+            port: process.env.DB_PORT || process.env.MYSQLPORT,
+            dialect: 'mysql',
+        }
+    );
+}
 
 module.exports = db;
-
